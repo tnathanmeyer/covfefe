329b682fa82d10472547b33baa387a1963bbf426
Nathan Meyer <55331718+tnathanmeyer@users.noreply.github.com>
2024-04-22 16:24:28 -0400

    Add light/dark mode awareness

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
 	import '../app.postcss';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+			if (event.matches) {
+				document.documentElement.classList.add('dark');
+			} else {
+				document.documentElement.classList.remove('dark');
+			}
+		});
+	});
 </script>
 
 <slot />
