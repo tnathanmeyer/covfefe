fe1e75aeb74863703f608f2be5fe0f42d9197c0f
Nathan Meyer <55331718+tnathanmeyer@users.noreply.github.com>
2024-04-22 16:01:49 -0400

    Buncha stuff, incl. ding dang fancy button hiding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,24 @@
 	const TRANSITION_TIME = 500;
 	let opacity1 = 0;
 	let opacity2 = 100;
+	let opacity3 = 100;
 	let name = '';
 	let stage = 0;
+	let aeropress = false;
+	let v60 = false;
+	let showButton = true;
+	let formHeight = '160px';
 
 	setTimeout(() => (opacity1 = 100), 250);
+
+	function hideButton() {
+		opacity3 = 0;
+		formHeight = '102px';
+
+		setTimeout(() => {
+			showButton = false;
+		}, TRANSITION_TIME);
+	}
 
 	function nextStage() {
 		opacity2 = 0;
@@ -19,39 +33,57 @@
 	$: if (stage === 1) setTimeout(nextStage, 3000);
 </script>
 
+<!-- with button, 160px
+	without button, 102px -->
+
 <div class="w-screen container h-full mx-auto flex justify-center items-center">
 	<div class="w-3/4 md:w-1/2 lg:w-1/3">
 		<div class="space-y-5 transition-all ease-in duration-1000 opacity-{opacity1}">
 			<h1 class="h1">Hello</h1>
 			<div class="space-y-5 transition-all ease-in duration-{TRANSITION_TIME} opacity-{opacity2}">
 				{#if stage === 0}
-					<form on:submit={nextStage} class="w-full card p-4 text-token space-y-4">
+					<form
+						on:submit={hideButton}
+						class="h-[{formHeight}] transition-all ease-in duration-{TRANSITION_TIME} w-full card p-4 text-token space-y-4"
+					>
 						<label class="label">
 							<span>What is your name?</span>
 							<input class="input" type="text" bind:value={name} required />
 						</label>
-						<div class="text-right">
-							<button type="submit" class="btn variant-outline">Submit</button>
-						</div>
+						{#if showButton}
+							<div class="text-right opacity-{opacity3}">
+								<button type="submit" class="btn variant-outline">Submit</button>
+							</div>
+						{/if}
 					</form>
 				{:else if stage === 1}
 					<p>Welcome, {name}</p>
 					<p>Today, we're going to choose your coffee gear.</p>
-				{:else}
-					<h3 class="h3">Brewer Types</h3>
-					<div class="w-full grid grid-cols-1 md:grid-cols-2 gap-4">
-						<div class="card bg-initial overflow-hidden card-hover">
-							<img
-								class="w-full aspect-[1/1] object-cover"
-								src="https://aeropress.com/cdn/shop/files/APpresserAmazonheroimages_20231119-23_1300x.jpg?v=1700437038"
-								alt="Aeropress"
-							/>
-							<!-- <section class="p-4">
-							Aeropress
-						</section> -->
+				{:else if stage === 2}
+					<h3 class="h3">Select your brewers</h3>
+					<form on:submit={nextStage} class="w-full card p-4 space-y-4">
+						<p>First, select which brewers you would like to use</p>
+						<div class="space-y-2">
+							<label class="flex items-center space-x-2">
+								<input class="checkbox" type="checkbox" bind:checked={aeropress} />
+								<p>Aeropress</p>
+							</label>
+							<label class="flex items-center space-x-2">
+								<input class="checkbox" type="checkbox" bind:checked={v60} />
+								<p>Hario V60</p>
+							</label>
+							<div class="text-right">
+								<button type="submit" class="btn variant-outline">Submit</button>
+							</div>
 						</div>
-						<div class="card bg-initial p-4 flex justify-center items-center">Hario V60</div>
-					</div>
+					</form>
+				{:else if stage === 3}
+					{#if aeropress}
+						<p>You chose Aeropress, nice</p>
+					{/if}
+					{#if v60}
+						<p>You chose v60, are you sure?</p>
+					{/if}
 				{/if}
 			</div>
 		</div>
